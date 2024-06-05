3474b440fcbac994763a315dee0f4d1914280343
javoh1rqaxramonov <ubotirov22@.com>
2024-06-05 13:49:25 +0500

    finished

---
+++ index.js
@@ -0,0 +1,93 @@
+// let a=4;
+
+// switch (a) {
+//     case 1:
+//         console.log("bir");
+//         break;
+//         case 1:
+//         console.log("bir");
+//         break;
+//         case 1:
+//         console.log("bir");
+//         break;
+//         case 1:
+//         console.log("bir");
+//         break;
+
+//     default:
+//         break;
+// }
+// let a=5
+// switch (a) {
+//     case 1:
+//         console.log("dushnba");
+//         break;
+//         case 2:
+//             console.log("seshanba");
+//             break;
+
+//             case 3:
+//         console.log("chorshanba");
+//         break;
+//         case 4:
+//         console.log("payshanba");
+//         break;
+//         case 5:
+//         console.log("juma");
+//         break;
+
+//     default:
+//         console.log("mavjud emas");
+//         break;
+// }
+//  let a=6;
+//  switch (a) {
+//     case 1:
+//         console.log("qish");
+//         break;
+//         case 2:                                                                                      
+//         console.log("qish");
+//         break;
+//         case 12:
+//         console.log("qish");
+//         break;
+ 
+//     default:
+//         break;
+//  }
+// let j=1;
+// while(j<=10){
+//     console.log(j);
+//     j++;
+// }
+// let j=1;
+// let n=30;
+//  let result =1;
+// while(j<=n){
+
+// }
+// let a=15;
+// let b=30;
+// for(let i=1;i<=b;i++){
+//     if (a%3==0 && b%5==0) {
+        
+//     }
+// }
+// let a=12345;
+// let i=1;
+// while (i<=a) {
+//     if (a/100) {
+//         console.log(a);
+//     }
+// }
+// function greet() {
+ 
+//     console.log("hello");
+// }
+
+// greet();
+// function sum(arg1=1,arg2=1,) {
+//     let s= arg1 +arg2;
+//     return s;    
+// }
+// let result = sum(3,4);
